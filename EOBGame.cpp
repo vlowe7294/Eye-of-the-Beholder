c2edf4d383d6135300f1902222f65f5b54c2d770
vlowe7294 <vlowe7294@gmail.com>
2017-11-22 12:05:36 -0600

    Add files via upload

--- EOBGame.cpp
+++ EOBGame.cpp
@@ -1,15 +1,28 @@
 #include "stdafx.h"
 #include "EOBGame.h"
 
+ROOM map[2] = 
+{  
+	{  {2, 0, 0, 0}, {'n', 'x', 'x', 'x'}, NULL},
+	{  {1, 0, 0, 0}, {'x', 's', 'x', 'x'}, NULL}
+};
 
 
-EOBGame::EOBGame(void)
+
+
+
+EOBGame::EOBGame()
 {
+	m_playerLoc.y = m_playerLoc.x = 0;
 }
 
 
-EOBGame::~EOBGame(void)
+EOBGame::~EOBGame()
 {
+	delete map[0].pDoor;
+	map[0].pDoor = NULL;
+	delete map[1].pDoor;
+	map[1].pDoor = NULL;
 }
 
 
@@ -22,13 +35,15 @@ void EOBGame::Start()
 {
 	int chc = 0;
 
-	EOBCharacter* pChar = new EOBCharacter();
-	pChar->NewCharacter();
-	delete pChar;
-
+	if (map[0].pDoor == NULL)
+	{
+		map[0].pDoor = new EOBDoor();
+		map[1].pDoor = new EOBDoor();
+	}
+	
+	GameLoop();
 	return;
 
-	
 	while (chc != 3)
 	{
 		_wsystem(L"CLS\n");
@@ -80,6 +95,77 @@ bool EOBGame::NewGame()
 
 }
 
+void EOBGame::GameLoop()
+{
+	bool bGameEnded = false, bLook = true, bIsValid = false;
+	wstring input;
+	EOBDoor* pDoor;
+	ROOM thisRoom = map[0];
+
+	pDoor = thisRoom.pDoor;
+
+	while (!bGameEnded)
+	{
+		if (bLook)
+		{
+			wprintf(L"You are on the first level of the sewers of Waterdeep.  Behind you, the exit tunnel has caved in.\nYou have no choice but to go forward.\n");
+			pDoor->Round(m_playerLoc);
+			bLook = false;
+
+		}
+
+		bIsValid = false;
+		wprintf(L"Your action?  ");
+		wcin >> input;
+		cout << endl;
+
+		if (pDoor != NULL)
+			bIsValid = pDoor->ParseCommand(input, m_playerLoc);
+
+		if (!bIsValid)
+		{
+			if (input.length() < 1)
+				input = L"xxx";
+
+			switch (input[0])
+			{
+				case 'q':
+					if (input == L"quit")
+					{
+						bGameEnded = true;
+						bIsValid = true;
+					}
+					break;
+
+				case 'n':
+					bIsValid = true;
+
+					if (thisRoom.exitDirs[0] == 'n')
+						bLook = true;
+					else
+						wprintf(L"You can't go that way\n");
+
+					break;
+
+				case 's':
+					bIsValid = true;
+
+					if (thisRoom.exitDirs[1] == 's')
+						bLook = true;
+					else
+						wprintf(L"You can't go that way\n");
+
+					break;
+
+			}
+		}
+
+		if (!bIsValid)
+			wprintf(L"I dont understand \"%s\"\n", input.c_str());
+
+	}
+}
+
 /**********************
 Created 11/17/2017
 returns a sum total of random numbers
@@ -91,7 +177,7 @@ int diceRoll(int sides, int nRolls)
 
 	if (!init)
 	{
-		srand(201711190819);
+		srand(201711200908);
 		init = true;
 	}
 
@@ -102,8 +188,95 @@ int diceRoll(int sides, int nRolls)
 }
 
 
+EOBDoor::EOBDoor()
+{
+	m_location.x = m_location.y = 0;
+	m_direction = 'n';
+	m_bIsOpen = false;
+}
+
+EOBDoor::~EOBDoor()
+{
+
+}
+
+void EOBDoor::Round(EOBLOCATION plrLoc)
+{
+	wstring openStr, dirStr, dirStr2;
+
+	if (!IsHere(plrLoc))
+		return;
+
+	if (m_bIsOpen)
+		openStr = L"open";
+	else
+		openStr = L"closed";
+
+	if (m_direction == 'n')
+		dirStr = L"North";
+	else
+		dirStr = L"South";
+
+	wprintf(L"%s is a %s door.\n", dirStr.c_str(), openStr.c_str());;
+
+}
+
+bool EOBDoor::IsHere(EOBLOCATION plrLoc)
+{
+	return (plrLoc.x == m_location.x && plrLoc.y == m_location.y);		
+
+}
+
+bool EOBDoor::ParseCommand(wstring cmd, EOBLOCATION loc)
+{
+	if (!IsHere(loc) )
+		return false;
+
+	wchar_t dir = m_direction;
+	wstring str = L" ";
+
+	str[0] = dir;
+
+	if (cmd == L"open")
+	{
+		m_bIsOpen = true;
+		wprintf(L"The door is open.\n");
+		return true;
+	}
+
+	
+	if (cmd == str)
+	{
+		if (!m_bIsOpen)
+		{
+			wprintf(L"That way is blocked\n");
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+
+const wchar_t EOBCharacter::m_statName[EOBCharacter::NewStat][30] = { {L"Strength" }, { L"Intelligence" }, { L"Wisdom" }};
+const wchar_t EOBCharacter::m_classNames[EOBCharacter::NewClass + 1][30] = { { L"Fighter" },{ L"Ranger" },{ L"<new>" } };
+const wchar_t EOBCharacter::m_raceNames[EOBCharacter::NewRace + 1][30] = { { L"Human" },{ L"Elf" },{ L"<new>" } };
+
 EOBCharacter::EOBCharacter()
 {
+	for (int i = 0; i < NewStat; i++)
+		m_stats[i] = diceRoll(6, 3);
+
+	m_stats[7] = diceRoll(6, 1);
+
+	if (m_stats[7] > NewClass)
+		m_stats[7] = Ranger;
+
+	m_stats[8] = diceRoll(6, 1);
+
+	if (m_stats[8] > NewRace)
+		m_stats[8] = Elf;
 
 }
 
@@ -120,11 +293,9 @@ returns a sum total of random numbers
 void EOBCharacter::NewCharacter()
 {
 	int chc = 1;
-	const int nStats = 2;
-	const wchar_t statName[nStats][30] = { {L"Strength" }, { L"Intelligence" }};
-	int stats[nStats];
-
-	SelectAlignment();
+	int nStats = NewStat;
+	
+	FinalEntry();
 	return;
 
 	while (chc == 1)
@@ -133,8 +304,8 @@ void EOBCharacter::NewCharacter()
 
 		for (int i = 0; i < nStats; i++)
 		{
-			stats[i] = diceRoll(6, 3);
-			wprintf(L"%-30.30s\t%02d\n", statName[i], stats[i]);
+			m_stats[i] = diceRoll(6, 3);
+			wprintf(L"%-30.30s\t%02d\n", m_statName[i], m_stats[i]);
 		}
 
 		wprintf(L"\n1>  Reroll\n");
@@ -158,6 +329,8 @@ void EOBCharacter::NewCharacter()
 
 	if (!SelectClass())
 		return;
+
+	SelectAlignment();
 }
 
 
@@ -168,15 +341,13 @@ User selects the race of the new character
 bool EOBCharacter::SelectRace()
 {
 	int chc = 0, nStats;
-	enum Race { Human, New };
-	const wchar_t raceNames[New + 1][30] = { { L"Human" },{ L"<new>" } };
-
-	nStats = New + 1;
+	
+	nStats = NewRace + 1;
 	_wsystem(L"CLS\n");
 
 	for (int i = 0; i < nStats; i++)
 	{
-		wprintf(L"%2d\t%-30.30s\n", i + 1, raceNames[i]);
+		wprintf(L"%2d\t%-30.30s\n", i + 1, m_raceNames[i]);
 	}
 
 	wprintf(L"%2d\tCancel\n", nStats + 1);
@@ -202,15 +373,14 @@ User selects the class of the new character
 bool EOBCharacter::SelectClass()
 {
 	int chc = 0, nClasses;
-	enum Class { Fighter, New };
-	const wchar_t classNames[New + 1][30] = { { L"Fighter" },{ L"<new>" } };
+	
 
-	nClasses = New + 1;
+	nClasses = NewClass + 1;
 	_wsystem(L"CLS\n");
 
 	for (int i = 0; i < nClasses; i++)
 	{
-		wprintf(L"%2d\t%-30.30s\n", i + 1, classNames[i]);
+		wprintf(L"%2d\t%-30.30s\n", i + 1, m_classNames[i]);
 	}
 
 	wprintf(L"%2d\tCancel\n", nClasses + 1);
@@ -237,8 +407,8 @@ User selects the Alignment of the character
 void EOBCharacter::SelectAlignment()
 {
 	int chc = 0, nAlignments;
-	enum Alignment { LawfulGood, New };
-	const wchar_t alignmentNames[New + 1][30] = { { L"Lawful Good" },{ L"<new>" } };
+	enum Alignment { LawfulGood, ChaoticGood, New };
+	const wchar_t alignmentNames[New + 1][30] = { { L"Lawful Good" },{ L"Chaotic Good" },{ L"<new>" } };
 
 	nAlignments = New + 1;
 	_wsystem(L"CLS\n");
@@ -257,5 +427,36 @@ void EOBCharacter::SelectAlignment()
 		cin >> chc;
 		cout << endl;
 	}
+
+}
+
+/**********************
+Created 11/19/2017
+Shows character stats and enters name, last chance to cancel
+***********************/
+void EOBCharacter::FinalEntry()
+{
+	int chc = 0;
+	wstring name;
+	_wsystem(L"CLS\n");
+
+	for (int i = 0; i < NewStat; i++)
+		wprintf(L"%-30.30s\t%02d\n", m_statName[i], m_stats[i]);
+
+	wprintf(L"\n%s %s", m_raceNames[ m_stats[8] ], m_classNames[ m_stats[7] ] );
+
+	wprintf(L"\n1>  Keep\n");
+	wprintf(L"2>  Cancel\n");
+	
+	while (chc < 1 || chc > 2)
+	{
+		wprintf(L"Enter choice:  ");
+		cin >> chc;
+		cout << endl;
+	}
+
+	wprintf(L"Character Name:  ");
+	wcin >> name;
+	cout << endl;
 
 }
--- EOBGame.h
+++ EOBGame.h
@@ -8,10 +8,52 @@ public:
 	void NewCharacter();
 
 private:
+	enum Stat { Strength, Intelligence, Wisdom, NewStat };
+	const static wchar_t m_statName[NewStat][30];
+	enum Class { Fighter, Ranger, NewClass };
+	const static wchar_t m_classNames[NewClass + 1][30];
+	enum Race { Human, Elf, NewRace };
+	const static wchar_t m_raceNames[NewRace + 1][30];
+
+	int m_stats[10];
+
 	bool SelectRace();
 	bool SelectClass();
 	void SelectAlignment();
+	void FinalEntry();
 
+
+};
+
+struct EOBLOCATION
+{
+	int x;
+	int y;
+};
+
+class EOBDoor
+{
+public:
+	EOBDoor();
+	~EOBDoor();
+	bool IsHere(EOBLOCATION plrLoc);
+	void Round(EOBLOCATION plrLoc);
+	bool GetIsOpen() { return m_bIsOpen; };
+	void SetIsOpen(bool bOpen) { m_bIsOpen = bOpen; };
+	bool ParseCommand(wstring cmd, EOBLOCATION loc);
+
+private:
+	EOBLOCATION m_location;
+	char m_direction;
+	bool m_bIsOpen;
+
+};
+
+struct ROOM
+{
+	char exits[4];
+	char exitDirs[4];	
+	EOBDoor* pDoor;
 };
 
 class EOBGame
@@ -20,7 +62,10 @@ public:
 	EOBGame();
 	~EOBGame();
 	void Start();
+	void GameLoop();
 	bool NewGame();
+
+private:
+	EOBLOCATION m_playerLoc;
 	
 };
-
